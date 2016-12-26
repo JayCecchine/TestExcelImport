f38c7b2a2cf4fb19800b5490b07bc27b5cd100d1
Yohoho <Yohoho@YOHOHO-PC>
2016-12-25 22:21:03 -0500

    data table

--- TestExcelImport/Form1.Designer.vb
+++ TestExcelImport/Form1.Designer.vb
@@ -22,9 +22,66 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(53, 97)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(286, 31)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(127, 47)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 44)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Generate excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(146, 134)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(66, 198)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 393)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- TestExcelImport/Form1.vb
+++ TestExcelImport/Form1.vb
@@ -1,3 +1,91 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Office.Interop.Excel
+Imports System.Data
+Public Class Form1
+    Sub TestExcelImport()
 
+        Dim table As System.Data.DataTable = GetTable()
+        ' Create new Application.
+        Dim excel As Application = New Application
+
+        ' Open Excel spreadsheet.
+        Dim w As Workbook = excel.Workbooks.Open("C:\Excel\test.xlsx")
+        ' Dim Finals, Dimen1, Dimen2 As String
+
+        Dim b As String = w.Sheets(1).Range("A3").Value.ToString()
+
+        TextBox2.Text = b
+        ' Loop over all sheets.
+        For i As Integer = 1 To w.Sheets.Count
+
+            ' Get sheet.
+            Dim sheet As Worksheet = w.Sheets(i)
+
+            ' Get range.
+            Dim r As Range = sheet.UsedRange
+
+
+            ' Load all cells into 2d array.
+            Dim array(,) As Object = r.Value(XlRangeValueDataType.xlRangeValueDefault)
+            'Dim butts As String = r.Value(XlRangeValueDataType.xlRangeValueDefault)
+            'TextBox1.Text = butts
+            ' Scan the cells.
+            If array IsNot Nothing Then
+                Debug.WriteLine("Length: {0}", array.Length)
+
+                ' Get bounds of the array.
+                Dim bound0 As Integer = array.GetUpperBound(0)
+                Dim bound1 As Integer = array.GetUpperBound(1)
+
+
+
+
+
+                ' Loop over all elements.
+                For j As Integer = 1 To bound0
+                    For x As Integer = 1 To bound1
+                        Dim s1 As String = array(j, x)
+                        Debug.Write(s1)
+                        Debug.Write(" "c)
+
+                    Next
+                    Debug.WriteLine("")
+                Next
+            End If
+
+        Next
+        ' Dimen1 = "Dimension 0: " &
+        'Dimen2 = "Dimension 1: " &
+        'Finals = Dimen1 & Dimen2
+        'TextBox1.Text = b
+        ' Close.
+
+        w.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataGridView1.DataSource = GetTable()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Function GetTable() As System.Data.DataTable
+        ' Create new DataTable instance.
+        Dim table As New System.Data.DataTable
+
+        ' Create four typed columns in the DataTable.
+        table.Columns.Add("Dosage", GetType(Integer))
+        table.Columns.Add("Drug", GetType(String))
+        table.Columns.Add("Patient", GetType(String))
+        table.Columns.Add("Date", GetType(DateTime))
+
+        ' Add five rows with those columns filled in the DataTable.
+        table.Rows.Add(25, "Indocin", "David", DateTime.Now)
+        table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now)
+        table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now)
+        table.Rows.Add(21, "Combivent", "Janet", DateTime.Now)
+        table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now)
+        Return table
+    End Function
 End Class
