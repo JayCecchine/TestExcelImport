3319ce243cf68a352bbc2f09c82ab9295942cb2f
jason.cecchine <jason.cecchine@CALLCENTER704>
2016-12-28 11:20:27 -0500

    datatable6

--- TestExcelImport/Form1.Designer.vb
+++ TestExcelImport/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,20 +20,18 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(11, 108)
+        Me.TextBox1.Location = New System.Drawing.Point(69, 63)
         Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
@@ -42,7 +40,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(96, 68)
+        Me.Button1.Location = New System.Drawing.Point(151, 23)
         Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(110, 36)
@@ -52,7 +50,7 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 164)
+        Me.TextBox2.Location = New System.Drawing.Point(70, 119)
         Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
@@ -62,7 +60,7 @@ Partial Class Form1
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(60, 245)
+        Me.DataGridView1.Location = New System.Drawing.Point(11, 200)
         Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader
@@ -70,25 +68,11 @@ Partial Class Form1
         Me.DataGridView1.Size = New System.Drawing.Size(386, 158)
         Me.DataGridView1.TabIndex = 3
         '
-        'OpenFileDialog1
-        '
-        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(12, 12)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(54, 27)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(501, 524)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(427, 364)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Button1)
@@ -106,6 +90,4 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents Button2 As Button
 End Class
--- TestExcelImport/Form1.vb
+++ TestExcelImport/Form1.vb
@@ -1,13 +1,16 @@
 ﻿Imports Microsoft.Office.Interop.Excel
 Imports System.Data
 Public Class Form1
-
+    Public Shared Function IsNullOrEmpty(
+    value As String
+) As Boolean
+    End Function
     Sub TestExcelImport()
 
         Dim openFile = New OpenFileDialog
         openFile.Title = "Select an Excel File"
         openFile.Filter = "Excel Files|*.xls;*.xlsx|All Files|*.*"
-        If openFile.ShowDialog() <> True Then
+        If openFile.ShowDialog() <> DialogResult.OK Then
             Return
         End If
 
@@ -29,7 +32,13 @@ Public Class Form1
             For r As Integer = 1 To firstSheet.Rows.Count
                 Dim newRow As DataRow = returnTable.NewRow()
                 For c As Integer = 1 To firstSheet.Columns.Count
-                    newRow(c - 1) = firstSheet.Cells(r, c).Value.ToString()
+
+                    If IsNullOrEmpty(firstSheet.Cells(r, c).Value.ToString()) Then
+                        newRow(c - 1) = ""
+                    Else
+                        newRow(c - 1) = firstSheet.Cells(r, c).Value.ToString()
+                    End If
+
                 Next
                 returnTable.Rows.Add(newRow)
                 Console.WriteLine(String.Format("Read {0} row(s) from sheet {1}.", r - 1, s))
@@ -46,75 +55,11 @@ Public Class Form1
 
     End Sub
 
-    Sub TestExcelImport2()
-        
-        ' Open Excel spreadsheet.
-        'Dim FilePath = OpenFileDialog1.FileName
-        Dim openFile = New OpenFileDialog
-        openFile.Title = "Select an Excel File"
-        openFile.Filter = "Excel Files|*.xls;*.xlsx|All Files|*.*"
-        If openFile.ShowDialog() <> DialogResult.OK Then
-            Return
-        End If
-
-        ' Create new Application.
-        Dim excel As Application = New Application
-        Dim w As Workbook = excel.Workbooks.Open(openFile.FileName)
-        Dim jasonsSet As New DataSet
-
-        For i As Integer = 1 To w.Sheets.Count
-
-            Dim sheet As Worksheet = w.Sheets(i)
-            Dim newTable As New System.Data.DataTable
-            newTable.TableName = String.Format("Sheet{0}", i)
-            Dim thisRange As Range = sheet.UsedRange
-
-
-            For x As Integer = 1 To thisRange.Columns.Count
-                Dim newCol As New DataColumn
-                newCol.ColumnName = String.Format("Column{0}", x)
-                newTable.Columns.Add(newCol)
-            Next
-            For r As Integer = 1 To thisRange.Rows.Count
-                Dim newRow As DataRow = newTable.NewRow()
-                For c As Integer = 1 To thisRange.Columns.Count
-                    newRow(c - 1) = thisRange.Cells(r, c).Value.ToString()
-                Next
-                newTable.Rows.Add(newRow)
-            Next
-            jasonsSet.Tables.Add(newTable)
-        Next
-
-        ' Dim array(,) As Object = r.Value(XlRangeValueDataType.xlRangeValueDefault)
-        'Get used range of selected sheet
-        'Make a for loop array that gets the data type and value of 
-        '   the selected cell, i.e 1,1 , would minus by -1 
-        '       and convert into a data table via add row.
-        'Figure out how to create the columns, as the columns decide
-        'http://stackoverflow.com/questions/23004274/vb-net-excel-worksheet-cells-value
-        'table.Columns.AddRange()
-
-        ' Dim array(,) As Object = r.Value(XlRangeValueDataType.xlRangeValueDefault)
-
-
-        'Dim c As String = w.Sheets(1).range(r).Value.ToString()
-        'TextBox2.Text = c
-
-        'Have to figure out how to construct a data tabel from the excel file.
-        'this might help http://stackoverflow.com/questions/14261655/best-fastest-way-to-read-an-excel-sheet-into-a-datatable
-
-
-
-        DataGridView1.DataSource = jasonsSet.Tables(0)
-        w.Close()
-        excel.Quit()
-
-    End Sub
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
-            TestExcelImport2()
+            TestExcelImport()
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
@@ -129,28 +74,9 @@ Public Class Form1
         tt.Columns.Add("An", GetType(String))
         tt.Columns.Add("ba", GetType(String))
     End Function
-    Function GetTable() As System.Data.DataTable
-        ' Create new DataTable instance.
-        Dim pharmacy As New System.Data.DataTable
 
-        '' Create four typed columns in the DataTable.
-        pharmacy.Columns.Add("Dosage", GetType(Integer))
-        pharmacy.Columns.Add("Drug", GetType(String))
-        pharmacy.Columns.Add("Patient", GetType(String))
-        pharmacy.Columns.Add("Date", GetType(DateTime))
+    Sub Button2_Click(sender As Object, e As EventArgs)
 
-
-        ' Add five rows with those columns filled in the DataTable.
-        pharmacy.Rows.Add(25, "Indocin", "David", DateTime.Now)
-        pharmacy.Rows.Add(50, "Enebrel", "Sam", DateTime.Now)
-        pharmacy.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now)
-        pharmacy.Rows.Add(21, "Combivent", "Janet", DateTime.Now)
-        pharmacy.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now)
-        Return pharmacy
-    End Function
-
-    Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        OpenFileDialog1.ShowDialog()
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
