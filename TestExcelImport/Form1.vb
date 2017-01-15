cb28c41ae8bd9b3bdde31f7272b72680120b23ca
jason.cecchine <jason.cecchine@CALLCENTER704>
2017-01-15 14:50:47 -0500

    Catering Fee Dilemma

--- TestExcelImport/Form1.vb
+++ TestExcelImport/Form1.vb
@@ -1,8 +1,7 @@
 ﻿Imports Microsoft.Office.Interop.Excel
 Imports System.Data
 Imports System.Runtime.InteropServices
-
-
+Imports System.Threading
 
 Public Class Form1
     Dim returnSet As New DataSet
@@ -19,13 +18,19 @@ Public Class Form1
         Clipboard.SetDataObject(strCopyMe)
 
         'open notepad and wait for it to completely load
-        Dim p As System.Diagnostics.Process = System.Diagnostics.Process.Start("notepad.exe")
+        'Dim p As System.Diagnostics.Process = System.Diagnostics.Process.Start("notepad.exe")
+        Dim p As New Process()
+        p.StartInfo.FileName = "notepad.exe"
+        p.Start()
+        Thread.Sleep(100)
+        SetWindowText(p.MainWindowHandle, "Test")
         p.WaitForInputIdle()
-
 
         ' paste the data from the clipboard
         SendKeys.Send("^V")
     End Sub
+    Shared Function SetWindowText(ByVal hwnd As IntPtr, ByVal windowName As String) As Boolean
+    End Function
     Function TestExcelImport()
 
         Dim openFile = New OpenFileDialog
@@ -214,6 +219,8 @@ Public Class Form1
         Dim u As String = "update tblMenuItems set Price1= '{0}' "
         Dim regPrice(52) As String
         Dim catPrice(52) As String
+        Dim regFee(52) As String
+        Dim catFee(52) As String
         Dim m = modSet.Tables(0)
 
 
@@ -259,9 +266,14 @@ Public Class Form1
         '   I am just ripping the value off of the cheese entry, will split them later and get
         '       The itemnums from tblmenuitems in sql
         If m.Rows(8).Item(1).ToString() IsNot "" Then
-            regPrice(8) = String.Format("--Avocado & Cheese--" & vbNewLine & u & "where itemnum in (201,211,102,212,213,118,219,102,220)" & vbNewLine, m.Rows(8).Item(1).ToString())
+            regPrice(8) = String.Format("--Cheese--" & vbNewLine & u & "where itemnum in (118,212,219,211)" & vbNewLine, m.Rows(8).Item(1).ToString())
         Else
             regPrice(8) = ""
+        End If
+        If m.Rows(9).Item(1).ToString() IsNot "" Then
+            regPrice(9) = String.Format("--Avocado--" & vbNewLine & u & "where itemnum in (102,201,213,220)" & vbNewLine, m.Rows(9).Item(1).ToString())
+        Else
+            regPrice(9) = ""
         End If
         'Future Avocado entry here, m.row(9).item(1)
         If m.Rows(10).Item(1).ToString() IsNot "" Then
@@ -362,6 +374,92 @@ Public Class Form1
             catPrice(10) = ""
         End If
 
+        '
+        '
+        'Regular Fees
+        '
+        '
+        If m.Rows(0).Item(1).ToString() IsNot "" Then
+            regFee(0) = String.Format("--Slims--" & vbNewLine & u & "where itemnum in (124,141,142,143,144,145,146)" & vbNewLine, m.Rows(0).Item(1).ToString())
+        Else
+            regFee(0) = ""
+        End If
+        If m.Rows(1).Item(1).ToString() IsNot "" Then
+            regFee(1) = String.Format("--Subs--" & vbNewLine & u & "where itemnum in (110,111,112,113,114,115,116)" & vbNewLine, m.Rows(1).Item(1).ToString())
+        Else
+            regFee(1) = ""
+        End If
+        If m.Rows(2).Item(1).ToString() IsNot "" Then
+            regFee(2) = String.Format("--Clubs--" & vbNewLine & u & "where itemnum in (125,126,127,128,129,130,131,132,133,134,650)" & vbNewLine, m.Rows(2).Item(1).ToString())
+        Else
+            regFee(2) = ""
+        End If
+        If m.Rows(3).Item(1).ToString() IsNot "" Then
+            regFee(3) = String.Format("--Garg--" & vbNewLine & u & "where itemnum in (117)" & vbNewLine, m.Rows(3).Item(1).ToString())
+        Else
+            regFee(3) = ""
+        End If
+        If m.Rows(4).Item(1).ToString() IsNot "" Then
+            regFee(4) = String.Format("--Fresh Bread--" & vbNewLine & u & "where itemnum in (244)" & vbNewLine, m.Rows(4).Item(1).ToString())
+        Else
+            regFee(4) = ""
+        End If
+        If m.Rows(5).Item(1).ToString() IsNot "" Then
+            regFee(5) = String.Format("--Day Old Bread--" & vbNewLine & u & "where itemnum in (209)" & vbNewLine, m.Rows(5).Item(1).ToString())
+        Else
+            regFee(5) = ""
+        End If
+        If m.Rows(6).Item(1).ToString() IsNot "" Then
+            regFee(6) = String.Format("--Meats--" & vbNewLine & u & "where itemnum in (247,450,451,452,453,454,455,456,119,120,121,122,123,139,147,179,180,212,213,245)" & vbNewLine, m.Rows(6).Item(1).ToString())
+        Else
+            regFee(6) = ""
+        End If
+        If m.Rows(8).Item(1).ToString() IsNot "" Then
+            regFee(8) = String.Format("--Cheese--" & vbNewLine & u & "where itemnum in (118,212,219,211)" & vbNewLine, m.Rows(8).Item(1).ToString())
+        Else
+            regFee(8) = ""
+        End If
+        If m.Rows(9).Item(1).ToString() IsNot "" Then
+            regFee(9) = String.Format("--Avocado--" & vbNewLine & u & "where itemnum in (102,201,213,220)" & vbNewLine, m.Rows(9).Item(1).ToString())
+        Else
+            regFee(9) = ""
+        End If
+        If m.Rows(10).Item(1).ToString() IsNot "" Then
+            regFee(10) = String.Format("--Pickles--" & vbNewLine & u & "where itemnum in (162,182,183)" & vbNewLine, m.Rows(10).Item(1).ToString())
+        Else
+            regFee(10) = ""
+        End If
+        If m.Rows(11).Item(1).ToString() IsNot "" Then
+            regFee(11) = String.Format("--Chips--" & vbNewLine & u & "where itemnum in (221,222,223,224,255,256,257,259,258,598,675,597)" & vbNewLine, m.Rows(11).Item(1).ToString())
+        Else
+            regFee(11) = ""
+        End If
+        If m.Rows(12).Item(1).ToString() IsNot "" Then
+            regFee(12) = String.Format("--Medium Sodas--" & vbNewLine & u & "where [group]='6' and ItemDesc like '%med%'" & vbNewLine, m.Rows(12).Item(1).ToString())
+        Else
+            regFee(12) = ""
+        End If
+        If m.Rows(13).Item(1).ToString() IsNot "" Then
+            regFee(13) = String.Format("--Large Sodas--" & vbNewLine & u & "where [group]='6' and ItemDesc like '%lg%'" & vbNewLine, m.Rows(13).Item(1).ToString())
+        Else
+            regFee(13) = ""
+        End If
+        If m.Rows(14).Item(1).ToString() IsNot "" Then
+            regFee(14) = String.Format("--Canned Sodas--" & vbNewLine & u & "where [group]='6' and ItemDesc like '%can%'" & vbNewLine, m.Rows(14).Item(1).ToString())
+        Else
+            regFee(14) = ""
+        End If
+        If m.Rows(15).Item(1).ToString() IsNot "" Then
+            regFee(15) = String.Format("--Bottled Water--" & vbNewLine & u & "where itemnum in (155)" & vbNewLine, m.Rows(15).Item(1).ToString())
+        Else
+            regFee(15) = ""
+        End If
+        If m.Rows(16).Item(1).ToString() IsNot "" Then
+            regFee(16) = String.Format("--Cookies--" & vbNewLine & u & "where itemnum in (215,216,676)" & vbNewLine, m.Rows(16).Item(1).ToString())
+        Else
+            regFee(16) = ""
+        End If
+
         'Regular Prices
         m.Rows(1).Item(1).ToString()
         m.Rows(2).Item(1).ToString()
@@ -426,16 +524,27 @@ Public Class Form1
         m.Rows(10).Item(5).ToString()
 
         Me.regPrice = "use PDQPOS" & vbNewLine & "go" & vbNewLine & vbNewLine &
-        "--" & vbNewLine &
-        "--Regular Prices" &
-        "--" & vbNewLine & vbNewLine & regPrice(0) & regPrice(1) & regPrice(2) & regPrice(3) & regPrice(4) & regPrice(5) & regPrice(6) & regPrice(7) &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine &
+        "-------------------------------------------Regular Prices----------------------------------------------------" & vbNewLine &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine & regFee(0) & regFee(1) & regFee(2) & regFee(3) & regFee(4) & regFee(5) & regFee(6) & regFee(7) &
         regPrice(8) & regPrice(9) & regPrice(10) & regPrice(11) & regPrice(12) & regPrice(13) & regPrice(14) & regPrice(15) & regPrice(16) &
         regPrice(17) & vbNewLine &
-        "--" & vbNewLine &
-        "--Catering Prices" & vbNewLine &
-        "--" & vbNewLine &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine &
+        "-------------------------------------------Catering Prices---------------------------------------------------" & vbNewLine &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine &
         catPrice(0) & catPrice(1) & catPrice(2) & catPrice(3) & catPrice(4) & catPrice(5) & catPrice(6) &
-        catPrice(7) & catPrice(8) & catPrice(9) & catPrice(10)
+        catPrice(7) & catPrice(8) & catPrice(9) & catPrice(10) & vbNewLine &
+        vbNewLine &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine &
+        "--------------------------------------------Delivery Fees----------------------------------------------------" & vbNewLine &
+        "-------------------------------------------------------------------------------------------------------------" & vbNewLine &
+        regFee(0) & regFee(1) & regFee(2) & regFee(3) & regFee(4) & regFee(5) & regFee(6) &
+        regFee(7) & regFee(8) & regFee(9) & regFee(10)
+        '"--------------------------------------------Catering Fees----------------------------------------------------" & vbNewLine &
+        '"-------------------------------------------------------------------------------------------------------------" & vbNewLine &
+        'catPrice(0) & catPrice(1) & catPrice(2) & catPrice(3) & catPrice(4) & catPrice(5) & catPrice(6) &
+        'catPrice(7) & catPrice(8) & catPrice(9) & catPrice(10)
         NoteStart(Me.regPrice)
+
     End Function
 End Class
